1a13630424ca50576ada1659c1e575cb15093359
Furigane <freedominprogramming@gmail.com>
2025-01-18 16:35:31 +0300

    Sending asynchronous axios requests

--- src/Redux/slices/moviesSlice.js
+++ src/Redux/slices/moviesSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     films: [],
@@ -6,17 +7,35 @@ const initialState = {
     errors: null
 }
 
-export const fetchMovies = createAsyncThunk('movie/fetchmovies', async () => {
-    try {
-        const movies = await fetch("https://676c1c82bc36a202bb86c01d.mockapi.io/movies");
-        if(!movies.ok) {
-            throw new Error('Error on server, sorry');
-        }
-        return movies.json();
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+export const fetchMovies = createAsyncThunk('movie/fetchmovies', () => {
+    return axios.get("https://676c1c82bc36a202bb86c01d.mockapi.io/movies")
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            let errorMessage = '';
+            switch (error.response?.status) {
+                case 500:
+                    errorMessage = 'Произошла непонятная ошибка, скоро исправим';
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+            console.error(error);
+            throw new Error(errorMessage);
+        })
+    // try {
+    //     const movies = await fetch("https://676c1c82bc36a202bb86c01.mockapi.io/movies");
+    //     // if(!movies.ok) {
+    //     //     throw new Error('Error on server, sorry');
+    //     // }
+    //     return movies.json();
+    // } catch (error) {
+    //     console.error(error);
+    //     throw error;
+    // }
 })
 
 const moviesSlice = createSlice({
@@ -31,13 +50,13 @@ const moviesSlice = createSlice({
                 state.status = 'loading';
             }) 
             .addCase(fetchMovies.fulfilled, (state, action) => {
-                console.log(action.payload);
                 state.films = action.payload;
                 state.status = 'fulfilled';
             })
             .addCase(fetchMovies.rejected, (state, action) => {
                 state.status = 'loading';
                 state.errors = action.error.message;
+                console.log(action);
                 console.error(action.error.message)
             })
     }
